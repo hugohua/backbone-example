93a8cff5781da887047c65174788ccd1a8d12e08
hugo <baofen14787@gmail.com>
2014-07-07 00:17:21 +0800

    add all example

---
+++ backbone_marionette_require/js/routers/index.js
@@ -0,0 +1,12 @@
+/*global define */
+define([
+	'marionette'
+], function (Marionette) {
+	'use strict';
+
+	return Marionette.AppRouter.extend({
+		appRoutes: {
+			'*filter': 'setFilter'
+		}
+	});
+});
